e076889da1e2bf72332e7d50483a9b5cb7c1da62
Tristan Grovender <therealtristangrovender@gmail.com>
2020-03-05 15:11:48 -0600

    MVP complete. No stretch.

--- index.js
+++ index.js
@@ -174,7 +174,26 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {}
+class Student extends Lambdasian {
+    constructor(studentAttrs) {
+        super(studentAttrs);
+        this.previousBackground = studentAttrs.previousBackground;
+        this.className = studentAttrs.className;
+        this.favSubjects = studentAttrs.favSubjects;
+    }
+
+    listSubjects() {
+        return `Loving ${this.favSubjects}`;
+    }
+
+    PRAssignment(subject) {
+        return `${this.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}`;
+    }
+}
 
 /*
   TASK 6
@@ -189,7 +208,21 @@ class Student {}
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {}
+class ProjectManager extends Instructor {
+    constructor(managerAttrs) {
+        super(managerAttrs);
+        this.gradClassName = managerAttrs.gradClassName;
+        this.favInstructor = managerAttrs.favInstructor;
+    }
+
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
 
 /*
   STRETCH PROBLEM (no tests!)
